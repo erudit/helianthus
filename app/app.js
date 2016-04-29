d18d0872f844e17f795135a1410a728ba4e82e1a
Michael Aufreiter <michael@substance.io>
2016-04-29 15:37:34 -0400

    Implement HelianthusWriter.

--- app/app.js
+++ app/app.js
@@ -1,5 +1,9 @@
 var jQuery = require('jquery');
 
+var DocumentSession = require('substance/model/DocumentSession');
+var Component = require('substance/ui/Component');
+var HelianthusWriter = require('../src/HelianthusWriter');
+
 function _loadXML(path) {
   jQuery.ajax(path, {
     dataType: 'text'
@@ -8,7 +12,15 @@ function _loadXML(path) {
     var ArticleImporter = require('../src/ArticleImporter');
     var importer = new ArticleImporter();
     var doc = importer.importDocument(data);
-    debugger;
+
+    var documentSession = new DocumentSession(doc);
+    
+    // For debugging in the console
+    window.doc = doc;
+    Component.mount(HelianthusWriter, {
+      documentSession: documentSession
+    }, document.body);
+
   }.bind(this))
   .fail(function(xhr, status, err) {
     console.error(err);
