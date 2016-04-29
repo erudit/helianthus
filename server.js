60b34d07ca143af7eef2aee69a651bc5a1c35f24
Oliver Buchtala <oliver.buchtala@gmail.com>
2016-04-29 10:43:56 -0400

    Initial server setup.

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+var port = process.env.PORT || 5000;
+var server = require('substance/util/server');
+
+server.serveStyles(app, '/app.css', path.join(__dirname, 'app', 'app.scss'));
+server.serveJS(app, '/app.js', path.join(__dirname, 'app', 'app.js'));
+app.use(express.static(path.join(__dirname)));
+app.use('/fonts', express.static(path.join(__dirname, 'node_modules/font-awesome/fonts')));
+
+app.listen(port, function() {
+  console.log("Substance Examples running on port " + port);
+  console.log("http://127.0.0.1:"+port+"/");
+});
+
+module.exports = app;
