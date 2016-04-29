3fcf65b8de44c8e872f3e0f732fe9f7ef63d4af7
Oliver Buchtala <oliver.buchtala@gmail.com>
2016-04-29 11:08:22 -0400

    Load the sample.

--- app/app.js
+++ app/app.js
@@ -1,3 +1,17 @@
+var jQuery = require('jQuery');
+
+function _loadXML(path) {
+  jQuery.ajax(path, {
+    dataType: 'text'
+  })
+  .done(function(data){
+    debugger;
+  }.bind(this))
+  .fail(function(xhr, status, err) {
+    console.error(err);
+  });
+}
+
 window.onload = function() {
-  alert('Helianthus!');
+  _loadXML('data/sample.xml');
 };
